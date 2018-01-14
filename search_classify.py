82c2e636d2451e6e88af896344a4071a6ec99866
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2018-01-15 00:47:28 +0800

    Getting there ...

--- search_classify.py
+++ search_classify.py
@@ -10,61 +10,12 @@ from tqdm import tqdm
 from helper_functions import *
 import matplotlib.image as mpimg
 
-
 # Define a function to extract features from a single image window
 # This function is very similar to extract_features()
 # just for a single image rather than list of images
 
 # Define a function you will pass an image
 # and the list of windows to be searched (output of slide_windows())
-def single_img_features(img, color_space, spatial_size=(32, 32),
-                        hist_bins=32, orient=9,
-                        pix_per_cell=8, cell_per_block=2, hog_channel='ALL',
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
-    feature_image = None
-
-    # 1) Define an empty list to receive features
-    img_features = []
-    # 2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else:
-        feature_image = np.copy(img)
-    # 3) Compute spatial features if flag is set
-    if spatial_feat:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        # 4) Append features to list
-        img_features.append(spatial_features)
-    # 5) Compute histogram features if flag is set
-    if hist_feat:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        # 6) Append features to list
-        img_features.append(hist_features)
-    # 7) Compute HOG features if flag is set
-    if hog_feat:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:, :, channel],
-                                                     orient, pix_per_cell, cell_per_block,
-                                                     vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
-                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        # 8) Append features to list
-        img_features.append(hog_features)
-
-    # 9) Return concatenated array of features
-    return np.concatenate(img_features)
 
 
 car_images = glob.glob('data/vehicles/**/*.png', recursive=True)
@@ -143,21 +94,45 @@ print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
 # Check the prediction time for a single sample
 t = time.time()
 
-ystart = 400
-ystop = 656
-scale = 1.5
-
 
 def pipeline(img):
-    out_img, heat_map = find_cars(img, scale=scale, ystart=ystart, ystop=ystop,
-                                  pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                                  orient=orient, spatial_size=spatial_size, hist_bins=hist_bins,
-                                  X_scaler=X_scaler, svc=svc)
+    rectangles = []
 
-    heat_map = apply_threshold(heat_map, 1)
-    labels = label(heat_map)
+    for ystart, ystop, scale in [[400, 656, 1.5],
+                                 [400, 464, 1.0],
+                                 [416, 480, 1.0],
+                                 [400, 496, 1.5],
+                                 [432, 528, 1.5],
+                                 [400, 528, 2.0],
+                                 [432, 560, 2.0],
+                                 [400, 596, 3.5],
+                                 [464, 660, 3.5]]:
 
-    return draw_labeled_bboxes(np.copy(img), labels)
+        out_img, heat_map, rects = find_cars(img, scale=scale,
+                                             ystart=ystart,
+                                             ystop=ystop,
+                                             pix_per_cell=pix_per_cell,
+                                             cell_per_block=cell_per_block,
+                                             orient=orient,
+                                             spatial_size=spatial_size,
+                                             hist_bins=hist_bins,
+                                             X_scaler=X_scaler,
+                                             svc=svc)
+
+        rectangles.append(rects)
+
+    print(rectangles)
+
+    rectangles = [item for sublist in rectangles for item in sublist]
+
+    heatmap_img = np.zeros_like(img[:, :, 0])
+    heatmap_img = add_heat(heatmap_img, rectangles)
+    heatmap_img = apply_threshold(heatmap_img, 1)
+    labels = label(heatmap_img)
+    draw_img = draw_labeled_bboxes(np.copy(img), labels)
+
+    return draw_img
+
 
 video_file_name = "project_video.mp4"
 write_output = 'output_video/' + video_file_name
